5a9b6f8f44fa5dc546fb2cea251df828893aea5d
mannyk <man962@yahoo.com>
2023-01-21 13:56:27 -0800

    remove sys.path hack to import Connectivity

--- train_util.py
+++ train_util.py
@@ -4,8 +4,8 @@ import numpy as np
 from skimage.segmentation import mark_boundaries
 import cv2
 
-import sys
-sys.path.append('./third_party/cython/build/lib.win-amd64-3.8')
+#import sys
+#sys.path.append('./third_party/cython/build/lib.win-amd64-3.8')
 from connectivity import enforce_connectivity
 
 def init_spixel_grid(args,  b_train=True):
