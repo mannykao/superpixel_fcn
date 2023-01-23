fd8388983eaca51feed7c2ddc16eefadd445af29
Ujjawal .K. Panchal <ujjawalpanchal32@gmail.com>
2023-01-23 08:57:53 +0530

    feat: 1 req file only.

--- third_party/cython/setup.py
+++ third_party/cython/setup.py
@@ -1,9 +1,17 @@
 from distutils.core import setup
+import os
+
+requirements = ["Cython","numpy"]
+for requirement in requirements:
+  os.system(f"pip install {requirement}")
+
 from Cython.Build import cythonize
 import numpy
 
 setup(
   name = 'Connectivity',
+  install_requires = requirements,
   ext_modules = cythonize("connectivity.pyx"),
-  include_dirs=[numpy.get_include()]
+  include_dirs=[numpy.get_include()],
 )
+
